81c21b6668d1b0cb3b65dfa5afecd84567f4c4a7
exam <exam@Aptechnk1.edu>
2024-09-18 15:42:01 +0500

    sohaib

---
+++ lib/views/dashboardscreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
